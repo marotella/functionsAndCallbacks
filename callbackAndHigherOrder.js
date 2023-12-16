0e909635b3008d01cd1057fd7f16fa7b01d78466
marotella <michelle.a.rotella@gmail.com>
2023-12-16 15:40:28 -0500

    callback 1 completed

--- callbackAndHigherOrder.js
+++ callbackAndHigherOrder.js
@@ -6,15 +6,17 @@
 */
 
 // CODE HERE
-
+const multiply = (num1, num2, callit) => {
+    callit((num1*num2))
+}
 
 // UNCOMMENT THE FUNCTION CALL BELOW
 // RUN THIS FILE WITH NODE
 // CHECK YOUR ANSWER
 
-// multiply(4, 3, answer => {
-//   console.log('The answer is ' + answer) //should console.log 12
-// })
+multiply(4, 3, answer => {
+  console.log('The answer is ' + answer) //should console.log 12
+})
 
 
 
